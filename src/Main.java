a034ae4a01b1c189954b8f64474200e0ded3d894
Egor <user@MacBook-Air-User.local>
2017-03-25 23:33:15 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,86 @@
+/**
+ * Created by user on 18.03.17.
+ */
+
+import java.util.Scanner;// типо как include в плюсах//
+
+public class Main {
+    public static void main(String args[]) {
+        //System.out.println("Hello world!");
+        // Ctrl + Alt + L   - выровнять код  (Hot key)
+        // Ctrl + D    - копировать строку
+       /* Scanner scanner = new Scanner(System.in);
+
+
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+        double P = a + b + c;
+        //Math - класс хранения математ функций
+        //Math.sqrt - корень
+        double S = Math.sqrt((P/2- a) * (P/2 - b) * (P/2 - c));
+        System.out.println("P = " + P);
+        System.out.println("S = " + S);
+        */
+
+        //квадратное уравнение
+        Scanner scanner = new Scanner(System.in);
+
+
+        /*double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+        double d = b*b - 4 * a * c;
+        double x, x1, x2;
+        System.out.println("d = " + d);
+        if (d > 0) {
+            x1 = (-b + Math.sqrt(d)) / 2 * a; // d > 0
+            x2 = (-b - Math.sqrt(d)) / 2 * a; // d > 0
+            System.out.println("x1 = " + x1);
+            System.out.println("x2 = " + x2);
+        }
+        if ( d == 0) {
+            x = -b / 2 * a; // d = 0
+            System.out.println("x = " + x);
+        }
+        if ( d < 0) {
+            System.out.println("Корней нет. ");
+        }*/
+
+
+        //ВТОРАЯ_ЛЕКЦИЯ_DZ
+        //#1
+
+        /*System.out.println("Введите делимое число " );
+        double b = scanner.nextDouble();
+        System.out.println("Введите делитель (не равен 0)  " );
+        double c = scanner.nextDouble();
+        double x;
+        if(c == 0 )
+            System.out.println("Введеные данные не верны ( делитель = 0 )   " );
+        else {
+            x = b/c;
+            System.out.println("Результат деления : " + x );
+        }*/
+
+
+        //#2 (Дата основания Одессы)
+       /* int x = 1794;
+        System.out.println("В каком году была основана Одесса ? " );
+        double b = scanner.nextDouble();
+        if ( b == x )
+            System.out.println("Верно.  " );
+
+        else
+            System.out.println("НЕ верно. Одесса была основана в 1794 году.  " );
+
+        double c = scanner.nextDouble();
+        */
+
+       //3_LECTION
+
+
+
+    }
+}
+
+
